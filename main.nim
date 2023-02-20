7729c724ecd4ce75817189f98248dc52ef0b77d9
quantimnot <quantimnot@users.noreply.github.com>
2023-02-19 21:11:24 -0500

    Print envs of main and post

--- main.nim
+++ main.nim
@@ -1,5 +1,5 @@
 import
-  std/[jscore, jsconsole, jsffi, times]
+  std/[jscore, jsconsole, jsffi, times, os]
 
 let
   core = require"@actions/core"
@@ -8,6 +8,8 @@ let
 try:
   # `who-to-greet` input defined in action metadata file
   let nameToGreet = core.getInput"who-to-greet"
+  for env in envPairs():
+    echo env
   console.log(nameToGreet)
   # console.log &("Hello ", nameToGreet, "!")
   let time = getClockStr()
@@ -15,5 +17,5 @@ try:
   # Get the JSON webhook payload for the event that triggered the workflow
   # let payload = JSON.stringify(github.context.payload).to string
   # console.log &("The event payload: ", payload)
-except:
+except CatchableError:
   core.setFailed(getCurrentExceptionMsg())
