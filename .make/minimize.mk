b7f9618aeb6a43bfa9c8c8a2594721aea8ea8c75
quantimnot <quantimnot@users.noreply.github.com>
2023-02-19 21:39:17 -0500

    Ditch `pre` and `post`

--- .make/minimize.mk
+++ .make/minimize.mk
@@ -2,7 +2,3 @@ minimize_js = closure-compiler --js_output_file "$(@)"
 
 main.js: $(out)/bundle/main.js/index.js .make/minimize.mk .make/var.mk
 	$(minimize_js) "$(out)/bundle/main.js/index.js"
-pre.js: $(out)/bundle/pre.js/index.js .make/minimize.mk .make/var.mk
-	$(minimize_js) "$(out)/bundle/pre.js/index.js"
-post.js: $(out)/bundle/post.js/index.js .make/minimize.mk .make/var.mk
-	$(minimize_js) "$(out)/bundle/post.js/index.js"
